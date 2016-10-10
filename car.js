0a1ed5132494de15abf0e32be7833b1eb25ff94b
Braden CT Tan <tct13@yahoo.com>
2016-10-10 14:44:56 +0800

    accelerate added

--- car.js
+++ car.js
@@ -2,7 +2,11 @@ var Car = function (color, convertible) {
   this.convertible = convertible
   this.speed = 0
   this.color = color
-  
+
+  this.accelerate = function(num) {
+      this.speed += num
+  }
+
   this.decelerate = function (minusSpeed) {
     this.speed -= minumSpeed
   }
