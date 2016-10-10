126769bdb9d1922345439aae52d68e3068c708a3
Braden CT Tan <tct13@yahoo.com>
2016-10-10 14:35:27 +0800

    car.js amended

--- car.js
+++ car.js
@@ -2,6 +2,16 @@ var Car = function (color, convertible) {
   this.color = color
   this.speed = 0
   this.convertible = convertible
+
+
+  this.accelerate = function(num) {
+    this.speed += num
+  }
+
+  this.addColor = function(newColor) {
+    this.color = newColor
+  }
+
 }
 
 module.exports = Car
